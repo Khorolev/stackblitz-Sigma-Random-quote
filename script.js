5f8c782ff9078ed0337813c1bcdcd5daf190e859
Khorolev <maltonif@gmail.com>
2025-06-04 15:59:45 +0800

    with book covers

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function setQuote() {
 
+    const bookCover = document.getElementById("bookCover");
     const quote = document.getElementById("quote");
     const author = document.getElementById("author");
 
-    const quotes = ["The degree of civilization in a society can be judged by entering its prisons.  - Fyodor Dostoevsky",
-    "Man is a mystery. It needs to be unraveled, and if you spend your whole life unraveling it, don’t say that you’ve wasted time. I am studying that mystery because I want to be a human being. - Fyodor Dostoevsky"
+    const quotes = ["The degree of civilization in a society can be judged by entering its prisons.  - Fyodor Dostoevsky - Notes.jpg",
+    "Man is a mystery. It needs to be unraveled, and if you spend your whole life unraveling it, don’t say that you’ve wasted time. I am studying that mystery because I want to be a human being. - Fyodor Dostoevsky - Letters.jpg"
 ];
 
     const randomIndex = Math.floor(Math.random()*quotes.length);
@@ -12,6 +13,7 @@ function setQuote() {
 
     const quoteParts = randomQuote.split(" - ");
 
+    bookCover.src = quoteParts[2];
     quote.innerHTML = quoteParts[0];
     author.innerHTML = ` - ${quoteParts[1]}`;
 
