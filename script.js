4633de919f67c965b51a0362f7c2cda2ee7626a9
Khorolev <maltonif@gmail.com>
2025-06-04 15:36:57 +0800

    mvp

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ function setQuote() {
     const quote = document.getElementById("quote");
     const author = document.getElementById("author");
 
-    const randomQuote = "Man is a mystery. It needs to be unraveled, and if you spend your whole life unraveling it, don’t say that you’ve wasted time. I am studying that mystery because I want to be a human being. - Fyodor Dostoevsky";
+    const quotes = ["The degree of civilization in a society can be judged by entering its prisons.  - Fyodor Dostoevsky",
+    "Man is a mystery. It needs to be unraveled, and if you spend your whole life unraveling it, don’t say that you’ve wasted time. I am studying that mystery because I want to be a human being. - Fyodor Dostoevsky"];
+
+    const randomIndex = Math.floor(Math.random()*quotes.length);
+    const randomQuote = quotes[randomIndex];
 
     const quoteParts = randomQuote.split(" - ");
 
     quote.innerHTML = quoteParts[0];
-    author.innerHTML = " - ${quoteParts[1]}";
+    author.innerHTML = ` - ${quoteParts[1]}`;
 
 }
