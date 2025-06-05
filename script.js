c8d8e5fa025927ad0d3362b4eff8f5607ac5fc79
Khorolev <maltonif@gmail.com>
2025-06-05 14:31:58 +0800

    Used EventListener istead of button onlick

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-function setQuote() {
 
-    const bookCover = document.getElementById("bookCover");
-    const quote = document.getElementById("quote");
-    const author = document.getElementById("author");
 
-    const quotes = ["The degree of civilization in a society can be judged by entering its prisons.  - Fyodor Dostoevsky - Notes.jpg",
-    "Man is a mystery. It needs to be unraveled, and if you spend your whole life unraveling it, don’t say that you’ve wasted time. I am studying that mystery because I want to be a human being. - Fyodor Dostoevsky - Letters.jpg"
-];
+const button = document.getElementById("newQuote")
+const bookCover = document.getElementById("bookCover");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
 
-    const randomIndex = Math.floor(Math.random()*quotes.length);
-    const randomQuote = quotes[randomIndex];
+    button.addEventListener('click', () => {
+        
+        const quotes = ["The degree of civilization in a society can be judged by entering its prisons.  - Fyodor Dostoevsky - Notes.jpg",
+            "Man is a mystery. It needs to be unraveled, and if you spend your whole life unraveling it, don’t say that you’ve wasted time. I am studying that mystery because I want to be a human being. - Fyodor Dostoevsky - Letters.jpg"
+            ];
+    
+        const randomIndex = Math.floor(Math.random()*quotes.length);
+        const randomQuote = quotes[randomIndex];
+        
+        const quoteParts = randomQuote.split(" - ");
 
-    const quoteParts = randomQuote.split(" - ");
+        bookCover.src = quoteParts[2];
+        quote.innerHTML = quoteParts[0];
+        author.innerHTML = ` - ${quoteParts[1]}`;
 
-    bookCover.src = quoteParts[2];
-    quote.innerHTML = quoteParts[0];
-    author.innerHTML = ` - ${quoteParts[1]}`;
-
-}
+    });
